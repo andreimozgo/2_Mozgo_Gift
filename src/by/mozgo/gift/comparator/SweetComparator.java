b9888bcb8be0c51f649724806829cfed511a22e2
andreimozgo <andrei.mozgo@gmail.com>
2017-03-24 01:38:45 +0300

    Deleted method sort from comparator

--- src/by/mozgo/gift/comparator/SweetComparator.java
+++ src/by/mozgo/gift/comparator/SweetComparator.java
@@ -2,6 +2,7 @@ package by.mozgo.gift.comparator;
 
 import by.mozgo.gift.entity.AbstractSweet;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -16,11 +17,10 @@ public abstract class SweetComparator implements Comparator<AbstractSweet> {
         this.comparator = this;
     }
 
-    public abstract void sort(List<AbstractSweet> sweets);
-
     public void build(List<AbstractSweet> sweets, SweetChain chain) {
         if (chain.getComparators().contains(comparator)) {
-            sort(sweets);
+            Collections.sort(sweets, this);
+
         }
         if (nextComparator != null) {
             nextComparator.build(sweets, chain);
--- src/by/mozgo/gift/comparator/SweetSugarComparator.java
+++ src/by/mozgo/gift/comparator/SweetSugarComparator.java
@@ -2,9 +2,6 @@ package by.mozgo.gift.comparator;
 
 import by.mozgo.gift.entity.AbstractSweet;
 
-import java.util.Collections;
-import java.util.List;
-
 /**
  * @author Andrei Mozgo
  */
@@ -17,10 +14,5 @@ public class SweetSugarComparator extends SweetComparator {
     @Override
     public int compare(AbstractSweet sweet1, AbstractSweet sweet2) {
         return sweet1.getSugar() - sweet2.getSugar();
-    }
-
-    @Override
-    public void sort(List<AbstractSweet> sweets) {
-        Collections.sort(sweets, this);
     }
 }
